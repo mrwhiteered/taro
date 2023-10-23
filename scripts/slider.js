d909e5b4ff5994eff898bce8395958e7b7ca94a5
Daniil <68585455+mrwhiteered@users.noreply.github.com>
2023-10-23 13:44:58 +0400

    v3

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,6 +4,17 @@ const nextBtn = document.querySelector('.next__btn');
 const itemWidth = document.querySelector('.team__item').offsetWidth;
 const totalItems = document.querySelectorAll('.team__item').length;
 let currentPosition = 0;
+let screenWidth = window.innerWidth;
+
+let countPic = 4;
+
+if ( screenWidth<=460){
+    countPic = 2;
+} else if (screenWidth<=900) {
+ countPic =2
+} else {
+    countPic = 4;
+}
 
 prevBtn.addEventListener('click', () => {
     if (currentPosition < 0) {
@@ -13,7 +24,7 @@ prevBtn.addEventListener('click', () => {
 });
 
 nextBtn.addEventListener('click', () => {
-    if (currentPosition > -itemWidth * (totalItems - 4)) { // Определяем 4, так как у вас всегда отображается 4 элемента
+    if (currentPosition > -itemWidth * (totalItems - countPic)) { // Определяем 4, так как у вас всегда отображается 4 элемента
         currentPosition -= itemWidth;
         teamList.style.transform = `translateX(${currentPosition}px)`;
     }
